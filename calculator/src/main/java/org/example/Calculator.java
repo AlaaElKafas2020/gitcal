d44c1c44635fa7ac77376534aa0990b2db38a341
Alaa ElKaffass <icalaa.elkaffass@americanwell.com>
2023-03-01 18:17:42 +0100

    Added main

---
+++ calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class Calculator {
+
+    public BigInteger add(BigInteger a, BigInteger b) {
+        return a.add(b);
+    }
+
+    public BigInteger subtract(BigInteger a, BigInteger b) {
+        return a.subtract(b);
+    }
+
+    public BigInteger multiply(BigInteger a, BigInteger b) {
+        return a.multiply(b);
+    }
+
+    public BigInteger divide(BigInteger a, BigInteger b) {
+        return a.divide(b);
+    }
+}
--- calculator/src/main/java/org/example/Main.java
+++ calculator/src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Calculator cal= new Calculator();
+        System.out.println(cal.add(BigInteger.valueOf(900019372),BigInteger.valueOf(900019372)));
     }
 }
-//hi  ddd
-//hi  ddd
-//hi  ddd
-//hi  ddd
